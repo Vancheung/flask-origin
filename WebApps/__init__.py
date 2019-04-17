c81ed53bb2eb5643c0ac52161a34fb8e29f7bb8b
Vancheung <vancheung129@163.com>
2019-04-17 20:22:38 +0800

    Update __init__.py

--- WebApps/__init__.py
+++ WebApps/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
@@ -6,9 +6,21 @@ app = Flask(__name__)
 def get_index():
     return ("Hello world")
 
-@app.route('/user/<username>')
+@app.route('/user/<username>/')
 def get_index_username(username):
     return ("Hello, "+username)
+
+@app.route('/login',methods=['GET'])
+def get_login():
+    return render_template('Login_Index.html')
+
+@app.route('/login',methods=['POST'])
+def post_login():
+    username = request.form['username']
+    password = request.form['password']
+    if username =='admin' and password == 'password':
+        return render_template('Login_Success.html',username=username)
+    return render_template('Login_Index.html',message='Invalid User or Password',username=username)
 
 
 if __name__ == "__main__":
